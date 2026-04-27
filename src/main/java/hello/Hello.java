1f422792d6a2927b184b963fe60286b8d942c678
mkopsnc <michalo@man.poznan.pl>
2026-04-27 14:23:35 +0200

    Update Hello.java
    This is a change introduced inside Github.

--- src/main/java/hello/Hello.java
+++ src/main/java/hello/Hello.java
@@ -2,6 +2,8 @@ package hello;
 
 import io.vavr.collection.List;
 
+// Changes introduced inside Github
+
 public class Hello {
   public static void main(String ... args) {
     List.rangeClosed(1, 5)
